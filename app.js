5f6aec69f0e5fd83d372db00adfead791eb583ed
Monalipatra <163519248+Monalipatra@users.noreply.github.com>
2024-04-12 15:33:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 5000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! from samir');
+  res.send('Hello World! from Monali');
 });
 
 app.listen(5000, (err,res) => {
