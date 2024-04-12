07936b26caceb9763ff8c1f4dec076962f548236
Monalipatra <163519248+Monalipatra@users.noreply.github.com>
2024-04-12 15:30:01 +0530

    first

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World! from samir');
+});
+
+app.listen(5000, (err,res) => {
+ if (err) {
+       console.log(err)
+ } else {
+      console.log('Server is Running on port 5000')
+ }
+})
